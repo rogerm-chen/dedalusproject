c236486640d9639a8582e1712f98cbb1c0b67625
Roger Chen <45837011+theasphaltworld@users.noreply.github.com>
2021-10-20 21:26:59 +1300

    Update configure-portforwarding.ps1

--- configure-portforwarding.ps1
+++ configure-portforwarding.ps1
@@ -1,10 +1,36 @@
 param(
-		[parameter(ValueFromPipeline = $True, ValueFromPipelineByPropertyName = $True, Mandatory = $true,HelpMessage = "proxy informations")]
+		[parameter(ValueFromPipeline = $True, ValueFromPipelineByPropertyName = $True)]
 		$proxyinfosjson
     )
+$proxyinfosjson= '{"proxyinfos": {
+	"value": [
+		{
+			"listenaddress": "10.1.10.20",
+			"listenport": "443",
+			"connectaddress": "10.1.10.20",
+			"connectport": "443",
+			"firewallrulename": "port443",
+			"direction": "Inbound",
+			"action": "Allow",
+			"protocol": "TCP"
+		},
+		{
+			"listenaddress": "10.1.10.20",
+			"listenport": "80",
+			"connectaddress": "10.1.10.20",
+			"connectport": "80",
+			"firewallrulename": "port80",
+			"direction": "Inbound",
+			"action": "Allow",
+			"protocol": "TCP"
+		}
+	]
+}
+}'
 
 
-$proxyinfos=$proxyinfosjson |ConvertFrom-Json
+
+$proxyinfos= ConvertFrom-Json -InputObject $proxyinfosjson
 $proxyinfos=$proxyinfos.proxyinfos.value
 
 function set-proxy {
